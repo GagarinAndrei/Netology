515ee42df37d17cbf6b64293a7805eab33808fa7
Andrei <gagarin1982@gmail.com>
2021-11-02 17:20:29 +0500

    Пока на паузе, продолжу позже

--- 1_4_2/src/Main.java
+++ 1_4_2/src/Main.java
@@ -1,8 +1,45 @@
+import java.util.Scanner;
+
 public class Main {
 
 
-
     public static void main (String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        static int earnings = 0;
+        static int spendings = 0;
 
+        while (true) {
+            System.out.println("Выберите операцию и введите её номер");
+            System.out.println("1. Доход");
+            System.out.println("2. Расход");
+            System.out.println("3. Выбор системы налогообложения");
+
+            String input = scanner.nextLine();
+
+
+            if (input.equals("end")) {
+                break;
+            }
+            int operation = Integer.parseInt(input);
+            switch (operation) {
+                case 1:
+                    System.out.println("Введите сумму дохода:");
+                    String moneyStr = scanner.nextLine(); // Не используйте тут nextInt (!)
+                    int money = Integer.parseInt(moneyStr);
+                    earnings += money;
+                    break;
+                case 2:
+                    System.out.println("Введите сумму расхода:");
+                    moneyStr = scanner.nextLine();
+                    money = Integer.parseInt(moneyStr);
+                    spendings += money;
+                    break;
+                case 3:
+
+
+            }
+
+        }
+        System.out.println("Программа завершена");
     }
 }
