2473fd4b3e2ae9adf9a7ab803dda1d7c749e2769
Andrei <gagarin1982@gmail.com>
2021-11-03 15:17:05 +0500

    Ready!

--- 1_4_2/src/Main.java
+++ 1_4_2/src/Main.java
@@ -1,12 +1,44 @@
 import java.util.Scanner;
 
 public class Main {
+    static int earnings = 0;
+    static int spendings = 0;
 
+    public static int taxEarningOnly(int earnings) {
+        int tax = earnings * 6 / 100;
+        return tax;
+    }
+
+    public static int taxEarningSpanding(int earnings, int spendings) {
+        int tax = (earnings - spendings) * 15 / 100;
+        if (tax >= 0) {
+            return tax;
+        }
+        else {
+            return 0;
+        }
+    }
+
+    public static void comparisonTaxRate () {
+        if (taxEarningOnly(earnings) < taxEarningSpanding(earnings, spendings)) {
+            System.out.println("Советуем вам УСН доходы");
+            System.out.println("Ставка налога в ней составит: " + taxEarningOnly(earnings));
+            System.out.println("В другой системе: " + taxEarningSpanding(earnings, spendings));
+            System.out.println();
+        } else if (taxEarningOnly(earnings) > taxEarningSpanding(earnings, spendings)) {
+            System.out.println("Советуем вам УСН доходы минус расходы");
+            System.out.println("Ставка налога в ней составит: " + taxEarningSpanding(earnings, spendings));
+            System.out.println("В другой системе: " + taxEarningOnly(earnings));
+            System.out.println();
+        } else {
+            System.out.println("Можете выбрать любую систему, ставка налога одинаковая!");
+            System.out.println();
+        }
+
+    }
 
     public static void main (String[] args) {
         Scanner scanner = new Scanner(System.in);
-        static int earnings = 0;
-        static int spendings = 0;
 
         while (true) {
             System.out.println("Выберите операцию и введите её номер");
@@ -24,7 +56,7 @@ public class Main {
             switch (operation) {
                 case 1:
                     System.out.println("Введите сумму дохода:");
-                    String moneyStr = scanner.nextLine(); // Не используйте тут nextInt (!)
+                    String moneyStr = scanner.nextLine();
                     int money = Integer.parseInt(moneyStr);
                     earnings += money;
                     break;
@@ -35,11 +67,35 @@ public class Main {
                     spendings += money;
                     break;
                 case 3:
+                    System.out.println("Выберите систему налогообложения:");
+                    System.out.println("1. УСН доходы");
+                    System.out.println("2. УСН доходы минус расходы");
+                    input = scanner.nextLine();
+                        if (input.equals("end")) {
+                            break;
+                        }
+                        operation = Integer.parseInt(input);
+                        switch (operation) {
+                            case 1:
+                                System.out.println("Вы выбрали УСН доходы. Сумма налога: " + taxEarningOnly(earnings));
+                                comparisonTaxRate();
+                                break;
+                            case 2:
+                                System.out.println("Вы выбрали УСН доходы минус расходы. Сумма налога: " + taxEarningSpanding(earnings, spendings));
+                                comparisonTaxRate();
+                                break;
+                            default:
+                                break;
+                        }
+
+                    break;
+                default:
+                    System.out.println("Такой операции нет!");
 
 
             }
 
         }
-        System.out.println("Программа завершена");
+        System.out.println("Программа завершена.");
     }
 }
