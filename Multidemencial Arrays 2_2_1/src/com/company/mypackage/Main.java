f260fc8871ef01e1fafe8d4c96e3d0bf8781cd92
Andrei <gagarin1982@gmail.com>
2021-11-19 16:19:56 +0500

    2.2.1* complete

--- Multidemencial Arrays 2_2_1/src/com/company/mypackage/Main.java
+++ Multidemencial Arrays 2_2_1/src/com/company/mypackage/Main.java
@@ -1,6 +1,7 @@
 package com.company.mypackage;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
     final static int SIZE = 8;
@@ -16,8 +17,44 @@ public class Main {
         }
 
         printMatrix(matrix);
+        printLine();
+        printMenu();
+        printLine();
+        chooseTurn(matrix);
+    }
+
+    public static void chooseTurn(int[][] matrix) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("-> ");
+        int turnAngle = Integer.parseInt(scanner.nextLine());
+        printLine();
+
+        switch (turnAngle) {
+            case 1:
+                printMatrix(matrix);
+                break;
+            case 2:
+                printMatrix(rotateMatrix90(matrix));
+                break;
+            case 3:
+                printMatrix(rotateMatrix180(matrix));
+                break;
+            case 4:
+                printMatrix(rotateMatrix270(matrix));
+                break;
+        }
+    }
+
+    public static void printLine() {
         System.out.println("================================");
-        printMatrix(rotateMatrix(matrix));
+    }
+
+    public static void printMenu() {
+        System.out.println("Выберите угол поворота матрицы:");
+        System.out.println("1 - для поворота на 0 градусов");
+        System.out.println("2 - для поворота на 90 градусов");
+        System.out.println("3 - для поворота на 180 градусов");
+        System.out.println("4 - для поворота на 270 градусов");
     }
 
     public static void printMatrix(int[][] matrix) {
@@ -29,11 +66,28 @@ public class Main {
         }
     }
 
-    public static int[][] rotateMatrix(int[][] matrix) {
+    public static int[][] rotateMatrix90(int[][] matrix) {
         int[][] rotatedMatrix = new int[SIZE][SIZE];
         for (int i = 0; i < SIZE; i++) {
             for (int j = 0; j < SIZE; j++) {
                 rotatedMatrix[i][j] = matrix[(SIZE - 1) - j][i];
+            }
+        }
+        return rotatedMatrix;
+    }
+    public static int[][] rotateMatrix180(int[][] matrix) {
+        int[][] rotatedMatrix = new int[SIZE][SIZE];
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                rotatedMatrix[i][j] = matrix[(SIZE -1) - i][(SIZE -1) - j];
+            }
+        }
+        return rotatedMatrix;
+    }public static int[][] rotateMatrix270(int[][] matrix) {
+        int[][] rotatedMatrix = new int[SIZE][SIZE];
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                rotatedMatrix[i][j] = matrix[j][(SIZE - 1) - i];
             }
         }
         return rotatedMatrix;
