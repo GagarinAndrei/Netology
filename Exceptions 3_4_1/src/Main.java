c513b6889918a31fc28b998edbb31f6995d8da37
Andrei <gagarin1982@gmail.com>
2021-12-09 12:32:52 +0500

    Exceptions 3.4.1

---
+++ Exceptions 3_4_1/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        User vasya = new User("Vasya", "1234", 20);
+        User prtya = new User("Petya", "4321", 16);
+        User valentin = new User("Valintin", "password", 46);
+
+    }
+}
---
+++ Exceptions 3_4_1/src/User.java
@@ -0,0 +1,11 @@
+public class User {
+    private String login;
+    private String password;
+    private int age;
+
+    public User(String login, String password, int age) {
+        this.login = login;
+        this.password = password;
+        this.age = age;
+    }
+}
