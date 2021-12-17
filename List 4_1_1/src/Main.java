0b58b08793b6f926fd5b9ee179874da7bda316cb
Andrei <gagarin1982@gmail.com>
2021-12-18 00:01:52 +0500

    List 4.1.1 Ready

--- List 4_1_1/src/Main.java
+++ List 4_1_1/src/Main.java
@@ -4,19 +4,32 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
         Scanner scanner = new Scanner(System.in);
-        List<String> toDoList = new ArrayList<String>();
+        int menuItem;
 
+        List<String> toDoList = new ArrayList<>();
 
         while (true) {
             printMainMenu();
-            int menuItem = Integer.parseInt(scanner.nextLine());
+            menuItem = Integer.parseInt(scanner.nextLine());
             if (menuItem == 0) {
                 System.out.println("Программа завершена!");
                 break;
             }
-            actionChooser(menuItem);
+            switch (menuItem) {
+                case 1:
+                    System.out.println("Введите задачу:");
+                    toDoList.add(scanner.nextLine());
+                    break;
+                case 2:
+                    printToDoList(toDoList);
+                    break;
+                case 3:
+                    printToDoList(toDoList);
+                    System.out.println("Какую задачу удалить?");
+                    toDoList.remove((Integer.parseInt(scanner.nextLine()) + 1));
+                    break;
+            }
         }
     }
 
@@ -27,15 +40,15 @@ public class Main {
                 "0. Выход");
     }
 
-    public static void actionChooser(int menuItem) {
-        switch (menuItem) {
-            case 1:
-                break;
-            case 2:
-                break;
-            case 3:
-                break;
+    public static void printToDoList(List<String> list) {
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println((i + 1) + ". " + list.get(i));
         }
+        printLine();
+    }
+
+    public static void printLine() {
+        System.out.println("=======================");
     }
 }
 
