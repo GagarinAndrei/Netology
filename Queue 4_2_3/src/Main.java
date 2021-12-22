c0155761435fc88d0115465624e60fa7ae0ac7a3
Andrei <gagarin1982@gmail.com>
2021-12-22 13:58:14 +0500

    Queue 4.2.3 Ready

--- Queue 4_2_3/src/Main.java
+++ Queue 4_2_3/src/Main.java
@@ -1,5 +1,33 @@
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the formula");
+        String formula = scanner.nextLine();
+        String[] arrayValues = formula.split(" ");
+
+        ArrayDeque<String> sign = new ArrayDeque<>();
+        Queue<Integer> numbers = new ArrayDeque<>();
+
+        for (int i = 0; i < arrayValues.length; i ++) {
+            String value = arrayValues[i];
+            if (!(value.equals("+") | value.equals("-") | value.equals("/") | value.equals("*"))) {
+                numbers.add(Integer.parseInt(value));
+            } else {
+                sign.add(value);
+            }
+        }
+
+        while (!numbers.isEmpty()) {
+            System.out.print(numbers.poll() + " ");
+        }
+
+        while (!sign.isEmpty()) {
+            System.out.print(sign.pollLast() + " ");
+        }
 
     }
 }
